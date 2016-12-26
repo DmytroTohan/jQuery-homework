e3f333408f1bbc20b5b0789d02daf3a3881d5757
yuriigr <yuriigr@users.noreply.github.com>
2016-12-26 15:02:44 +0200

    upload task-3 folder

---
+++ task-3/js/script.js
@@ -0,0 +1,12 @@
+////testing
+//$(document).ready(function() {
+//  $("img").click(function(e) {
+//    $(this).zoomTarget({targetsize:0.5, scalemode:"height", duration:1000});
+//    e.stopPropagation();
+//  });
+//});
+
+//style fix
+$('.holder').css({"width": "65vw", "margin": "0 auto", "background": "#ffa338"});
+$('img').css({"width": "29vw", "height": "30vh", "padding": "1rem"});
+$('.holder div').css({"padding-left": "1.5vw"});
